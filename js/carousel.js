2e7584b5ed8b0d1394d95df699eb8e71bc4c8cc5
NestorGabriel0726 <gabriel.ramos0726@gmail.com>
2026-05-21 22:21:15 -0300

    Comentários adicionados no carousel.js, e alteração do html na página de lançamentos para ser inserida a div de comparação dos veículos.

--- js/carousel.js
+++ js/carousel.js
@@ -9,27 +9,29 @@ let carouselArr = [];
 //class Carousel
 class Carousel {
 
+    // Criação de objeto com tres propriedades para inserir no carrosel.
     constructor(image, title, link) {
         this.image = image;
         this.title = title;
         this.link = link;
     }
 
-    static _items = [];
-    static _sequence = 0;
-    static _size = 0;
-    static _interval = null;
+    static _slides = [];
+    static _indiceAtual = 0;
+    static _totalDeSlides = 0;
+    static _temporizador = null;
     
-      
+    // Inicia o carrosel de imagens.  
     static Start(arr){
         if(arr && Array.isArray(arr) && arr.length > 0) {
 
-            Carousel._items = arr;
-            Carousel._size = arr.length;
-            Carousel._sequence = 0;
+            // Configurando as variáveis da classe Carousel com os dados do array.
+            Carousel._slides = arr;
+            Carousel._totalDeSlides = arr.length;
+            Carousel._indiceAtual = 0;
 
-            Carousel.UpdateDOM();
-            Carousel.ResetTimer();
+            Carousel.UpdateDOM(); // Atualiza a tela fazendo o primeiro slide aparecer.
+            Carousel.ResetTimer(); // Inicia o temporizador.
             
             
         } else {
@@ -37,19 +39,22 @@ class Carousel {
         }
     }
 
+    // Lógica para o temporizador controlar o tempo de exibição de imagens do carrosel.
     static ResetTimer() {
-        clearInterval(Carousel._interval);
-        Carousel._interval = setInterval(function(){Carousel.Next();}, 2000);
+        clearInterval(Carousel._temporizador); // Para resetar o temporiizador caso o usuário clique em um dos botões.
+        Carousel._temporizador = setInterval(function(){Carousel.Next();}, 2000);
     }
 
+    // Lógica para passar o próximo slide.
     static Next(){
-        Carousel._sequence = (Carousel._sequence + 1) % Carousel._size;
+        Carousel._indiceAtual = (Carousel._indiceAtual + 1) % Carousel._totalDeSlides; // O operador % faz resetar para o indice 0 quando chega na última foto da carrosel.
         Carousel.UpdateDOM();
         Carousel.ResetTimer();
     }
 
+    // Lógica para retornar ao slide anterior.
     static Prev(){
-        Carousel._sequence = (Carousel._sequence - 1 + Carousel._size) % Carousel._size;
+        Carousel._indiceAtual = (Carousel._indiceAtual - 1 + Carousel._totalDeSlides) % Carousel._totalDeSlides;
         Carousel.UpdateDOM();
         Carousel.ResetTimer();
     }
@@ -59,7 +64,7 @@ class Carousel {
         const titleArea = document.getElementById("carousel-title")
         
         if (drawArea && titleArea) {
-            const currentItem = Carousel._items[Carousel._sequence];
+            const currentItem = Carousel._slides[Carousel._indiceAtual];
 
             drawArea.innerHTML = ` <a href= "${currentItem.link}">
                                     <img src= "${currentItem.image}" >
